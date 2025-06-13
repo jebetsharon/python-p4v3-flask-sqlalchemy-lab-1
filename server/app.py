9eefcdb1d09f312a8651b6f9b2a0342e032d4996
Sharon kosgei <jebetsharon089@gmail.com>
2025-06-13 12:26:33 +0300

    complete modifying views

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,6 +21,32 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    # __define-ocg__
+    eq = Earthquake.query.get(id)
+    if eq is None:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    return jsonify({
+        "id":        eq.id,
+        "location":  eq.location,
+        "magnitude": eq.magnitude,
+        "year":      eq.year
+    }), 200
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_quakes_by_magnitude(magnitude):
+    results = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes = [{
+        "id":        e.id,
+        "location":  e.location,
+        "magnitude": e.magnitude,
+        "year":      e.year
+    } for e in results]
+    return jsonify({
+        "count": len(quakes),
+        "quakes": quakes
+    }), 200
 
 
 if __name__ == '__main__':
